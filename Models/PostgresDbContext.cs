cddac3cbdc11dca852a915765cf90722d03bf259
Louis Devlin <72494457+Louis-Devlin@users.noreply.github.com>
2023-11-07 18:01:26 +0000

    Remove test collection from DbContext

--- Models/PostgresDbContext.cs
+++ Models/PostgresDbContext.cs
@@ -5,7 +5,6 @@ namespace Well_Up_API.Models
     public class PostgresDbContext : DbContext
     {
         public PostgresDbContext(DbContextOptions<PostgresDbContext> options) : base(options) { }
-        public DbSet<TestModel> Test { get; set; }
 
         public DbSet<Mood> Mood { get; set; }
 
