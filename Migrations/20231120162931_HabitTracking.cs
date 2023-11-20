bf20bfb65c5e562a77d2e126aba1cab73d06a0a4
Louis Devlin <72494457+Louis-Devlin@users.noreply.github.com>
2023-11-20 16:31:37 +0000

    WU-24: Create Habit and HabitLog tables
    Create Models for new classes, Update PostgresDbContext and existing models to create One to many relationships

---
+++ Migrations/20231120162931_HabitTracking.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Well_Up_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class HabitTracking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Habbit",
+                columns: table => new
+                {
+                    HabbitId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    HabbitName = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Habbit", x => x.HabbitId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HabitLog",
+                columns: table => new
+                {
+                    HabbitLogId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    HabbitId = table.Column<int>(type: "integer", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HabitLog", x => x.HabbitLogId);
+                    table.ForeignKey(
+                        name: "FK_HabitLog_Habbit_HabbitId",
+                        column: x => x.HabbitId,
+                        principalTable: "Habbit",
+                        principalColumn: "HabbitId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HabitLog_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HabitLog_HabbitId",
+                table: "HabitLog",
+                column: "HabbitId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HabitLog_UserId",
+                table: "HabitLog",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HabitLog");
+
+            migrationBuilder.DropTable(
+                name: "Habbit");
+        }
+    }
+}
