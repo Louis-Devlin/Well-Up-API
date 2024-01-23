69afe5928d3357a584635231c8daa2602ea83609
Louis Devlin <72494457+Louis-Devlin@users.noreply.github.com>
2024-01-23 12:36:27 +0000

    WU-37: Create MoodLogCount Model for response object

---
+++ Models/MoodLogCount.cs
@@ -0,0 +1,11 @@
+namespace Well_Up_API.Models
+{
+    public class MoodLogCountResponse
+    {
+        public int Count { get; set; }
+
+        public string Colour { get; set; }
+
+        public string MoodName { get; set; }
+    }
+}
