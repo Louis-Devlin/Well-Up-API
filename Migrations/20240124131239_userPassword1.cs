a379194d50260aba5328b07e09433b49067c8166
Louis Devlin <72494457+Louis-Devlin@users.noreply.github.com>
2024-01-24 13:24:35 +0000

    WU-40: Add Password field to User model

---
+++ Migrations/20240124131239_userPassword1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Well_Up_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class userPassword1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+        }
+    }
+}
