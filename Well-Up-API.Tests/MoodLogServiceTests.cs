edc6237bf66b7885285de7d6ea56ffc0444c578d
Louis Devlin <72494457+Louis-Devlin@users.noreply.github.com>
2024-03-29 20:45:56 +0000

    WU-82: Add test for weekly totals and totals by day

--- Well-Up-API.Tests/MoodLogServiceTests.cs
+++ Well-Up-API.Tests/MoodLogServiceTests.cs
@@ -8,32 +8,41 @@ namespace Well_Up_API.Tests
     [TestClass]
     public class MoodLogServiceTests
     {
-        private Mock<DbSet<MoodLog>> mockSet;
+        private Mock<DbSet<Mood>> mockMoodSet;
+        private Mock<DbSet<MoodLog>> mockMoodLogSet;
         private Mock<PostgresDbContext> mockContext;
         private MoodLogService mockService;
 
         [TestInitialize]
         public void Initialize()
         {
-            //Populate moods 
-            var moods = new List<Mood>(){
-                new Mood(){MoodId = 1, MoodName = "Happy"},
-                new Mood(){MoodId = 2, MoodName = "Sad"}
-            }.AsQueryable();
             var data = new List<MoodLog>(){
                 new MoodLog(){MoodLogId = 1, MoodId = 1, UserId = 1, Date = new DateTime(2024,3,29)},
                 new MoodLog(){MoodLogId = 2, MoodId = 2, UserId = 1, Date = new DateTime(2024,3,28)},
                 new MoodLog(){MoodLogId = 3, MoodId = 1, UserId = 2, Date = new DateTime(2024,3,29)}
             }.AsQueryable();
 
-            mockSet = new Mock<DbSet<MoodLog>>();
-            mockSet.As<IQueryable<MoodLog>>().Setup(m => m.Provider).Returns(data.Provider);
-            mockSet.As<IQueryable<MoodLog>>().Setup(m => m.Expression).Returns(data.Expression);
-            mockSet.As<IQueryable<MoodLog>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            mockSet.As<IQueryable<MoodLog>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+            mockMoodLogSet = new Mock<DbSet<MoodLog>>();
+            mockMoodLogSet.As<IQueryable<MoodLog>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockMoodLogSet.As<IQueryable<MoodLog>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockMoodLogSet.As<IQueryable<MoodLog>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockMoodLogSet.As<IQueryable<MoodLog>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var moodData = new List<Mood>(){
+                new Mood(){MoodId = 1, MoodName = "Happy", PositionX = 1, PositionY = 1},
+                new Mood(){MoodId = 2, MoodName = "Sad", PositionX = 2, PositionY = 2}
+            }.AsQueryable();
+
+            mockMoodSet = new Mock<DbSet<Mood>>();
+            mockMoodSet.As<IQueryable<Mood>>().Setup(m => m.Provider).Returns(moodData.Provider);
+            mockMoodSet.As<IQueryable<Mood>>().Setup(m => m.Expression).Returns(moodData.Expression);
+            mockMoodSet.As<IQueryable<Mood>>().Setup(m => m.ElementType).Returns(moodData.ElementType);
+            mockMoodSet.As<IQueryable<Mood>>().Setup(m => m.GetEnumerator()).Returns(() => moodData.GetEnumerator());
 
             mockContext = new Mock<PostgresDbContext>();
-            mockContext.Setup(c => c.MoodLog).Returns(mockSet.Object);
+            mockContext.Setup(c => c.Mood).Returns(mockMoodSet.Object);
+            mockContext.Setup(c => c.MoodLog).Returns(mockMoodLogSet.Object);
+
 
             mockService = new MoodLogService(mockContext.Object);
         }
@@ -67,20 +76,36 @@ namespace Well_Up_API.Tests
 
             mockContext.Setup(c => c.HabitLog).Returns(mockHabitSet.Object);
 
-            // For Mood
-            var moodData = new List<Mood>().AsQueryable();
-            var mockMoodSet = new Mock<DbSet<Mood>>();
 
-            mockMoodSet.As<IQueryable<Mood>>().Setup(m => m.Provider).Returns(moodData.Provider);
-            mockMoodSet.As<IQueryable<Mood>>().Setup(m => m.Expression).Returns(moodData.Expression);
-            mockMoodSet.As<IQueryable<Mood>>().Setup(m => m.ElementType).Returns(moodData.ElementType);
-            mockMoodSet.As<IQueryable<Mood>>().Setup(m => m.GetEnumerator()).Returns(moodData.GetEnumerator());
 
-            mockContext.Setup(c => c.Mood).Returns(mockMoodSet.Object);
 
             var userLog = mockService.GetByUser(1);
             Assert.AreEqual(2, userLog.Count);
             Assert.AreEqual(new DateTime(2024, 3, 29), userLog[0].Date);
+        }
+        [TestMethod]
+        public void GetWeeklyTotals_ShouldReturnDictionary()
+        {
+
+            var result = mockService.GetWeeklyTotals(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(Dictionary<string, int>));
+            Assert.AreEqual(4, result.Count); // Assuming there are 4 colors: red, yellow, blue, green
+        }
+
+        [TestMethod]
+        public void GetTotalsByDay_ShouldReturnListOfMoodLogCountResponse()
+        {
+            // Arrange
+            var userId = 1;
+            var date = DateTime.Now;
+
+            // Act
+            var result = mockService.GetTotalsByDay(userId, date);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(List<MoodLogCountResponse>));
         }
     }
 
